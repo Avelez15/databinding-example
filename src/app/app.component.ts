5e7c555105599ae4b89a3b8f770e5e3018ecacf1
Alejandro Velez <velez.alejandro4@gmail.com>
2023-07-28 19:50:54 -0400

    added databinding

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,11 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'databinding-example';
+  serverElements = [
+    { type: 'server', name: 'TestServer', content: 'Just a test' },
+    { type: 'server', name: 'gaymer', content: 'get rekt' },
+  ];
 }
